ae35fca3555d253710cd5c475465c91adb0bb8ee
ronak111091 <ronak.shettigar@gmail.com>
2017-05-10 21:34:27 -0400

    changed id from int to string

--- Project3/Models/Cast.cs
+++ Project3/Models/Cast.cs
@@ -8,7 +8,7 @@ namespace Project3.Models
 {
     public class Cast
     {
-        public int cast_id { get; set; }
+        public string cast_id { get; set; }
         public string character { get; set; }
         public string credit_id { get; set; }
         public string id { get; set; }
